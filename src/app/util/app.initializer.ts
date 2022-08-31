6a8cc4122d9a7e3de78c04c9076720dd4d1de30b
Cristhian Traversi <crist.trav@gmail.com>
2022-08-30 23:50:10 -0400

    Corregido app.initializer.ts

--- src/app/util/app.initializer.ts
+++ src/app/util/app.initializer.ts
@@ -1,13 +1,13 @@
 import { SesionService } from '../servicios/sesion.service';
 
 export function appInitializer(sessionSrv: SesionService){
-    return () => new Promise(resolve => {
+    return () => new Promise<void | null>(resolve => {
         const rtoken = localStorage.getItem('refreshToken');
         if(rtoken){
-            sessionSrv.refresh(rtoken).subscribe().add(resolve);
+            //sessionSrv.refresh(rtoken).subscribe().add(resolve);
+            sessionSrv.refresh(rtoken).subscribe(() => resolve());
         }else{
             resolve(null);
         }
-        
     });
 }
