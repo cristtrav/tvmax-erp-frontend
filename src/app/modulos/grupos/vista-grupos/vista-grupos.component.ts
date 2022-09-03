5610bbe23b535308a579ef1ec1da9ce37667083c
Cristhian Traversi <crist.trav@gmail.com>
2022-09-02 22:35:11 -0400

    Se agrego la funcionalidad de buscar Grupos por descripcion

--- src/app/modulos/grupos/vista-grupos/vista-grupos.component.ts
+++ src/app/modulos/grupos/vista-grupos/vista-grupos.component.ts
@@ -17,67 +17,85 @@ export class VistaGruposComponent implements OnInit {
   pageSize: number = 10;
   pageIndex: number = 1;
   totalRegisters: number = 1;
-  sort: string | null  = '+id';
+  sort: string | null = '+id';
   loadingData: boolean = false;
   lstGrupos: Grupo[] = [];
+  timerBusqueda: any;
+  textoBusqueda: string = '';
 
   constructor(
     private grupoSrv: GruposService,
     private notif: NzNotificationService,
     private httpErrorHandler: HttpErrorResponseHandlerService
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.cargarDatos();
   }
 
-  private cargarDatos(){
+  private cargarDatos() {
     this.loadingData = true;
-    this.grupoSrv.getGrupos(this.getQueryHttpParams()).subscribe((resp: ServerResponseList<Grupo>)=>{
-      this.lstGrupos = resp.data;
-      this.totalRegisters = resp.queryRowCount;
-      this.loadingData = false;
-    }, (e)=>{
-      this.loadingData = false;
-      console.log(`Error al cargar grupos`);
-      console.log(e);
-      this.httpErrorHandler.handle(e);
+    this.grupoSrv.getGrupos(this.getQueryHttpParams()).subscribe({
+      next: (resp) => {
+        this.lstGrupos = resp.data;
+        this.totalRegisters = resp.queryRowCount;
+        this.loadingData = false;
+      },
+      error: (e) => {
+        this.loadingData = false;
+        console.log(`Error al cargar grupos`);
+        console.log(e);
+        this.httpErrorHandler.handle(e);
+      }
     });
   }
 
-  eliminar(id: any): void{
-    this.grupoSrv.deleteGrupo(<number>id).subscribe(()=>{
+  eliminar(id: any): void {
+    this.grupoSrv.deleteGrupo(<number>id).subscribe(() => {
       this.cargarDatos();
       this.notif.create('success', 'Eliminado correctamente', '');
-    }, (e)=>{
+    }, (e) => {
       console.error(e);
       this.httpErrorHandler.handle(e);
     });
   }
 
-  onQueryParamsChange(params: NzTableQueryParams){
+  onQueryParamsChange(params: NzTableQueryParams) {
     this.pageIndex = params.pageIndex;
     this.pageSize = params.pageSize;
     this.sort = this.buildSort(params.sort);
     this.cargarDatos();
   }
 
-  buildSort(filters: {key: string, value: any}[]): string | null {
-    for(var f of filters){
-      if(f.value === 'ascend') return `+${f.key}`;
-      if(f.value === 'descend') return `-${f.key}`;
+  buildSort(filters: { key: string, value: any }[]): string | null {
+    for (var f of filters) {
+      if (f.value === 'ascend') return `+${f.key}`;
+      if (f.value === 'descend') return `-${f.key}`;
     }
     return null;
   }
 
   getQueryHttpParams(): HttpParams {
     var params: HttpParams = new HttpParams().append('eliminado', 'false');
-    params = params.append('offset', `${(this.pageIndex-1)*this.pageSize}`);
+    params = params.append('offset', `${(this.pageIndex - 1) * this.pageSize}`);
     params = params.append('limit', `${this.pageSize}`);
-    if(this.sort){
+    if (this.sort) {
       params = params.append('sort', this.sort);
     }
+    if(this.textoBusqueda) params = params.append('search', this.textoBusqueda);
     return params;
+  }
+
+  buscar() {
+    clearTimeout(this.timerBusqueda);
+    this.timerBusqueda = setTimeout(() => {
+      this.cargarDatos();
+    }, 500);
+  }
+
+  limpiarBusqueda(){
+    this.textoBusqueda = '';
+    this.cargarDatos();
   }
 
 }
