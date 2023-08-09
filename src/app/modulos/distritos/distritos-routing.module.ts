16b24d12f845c12bad355fd1c399a65d752208a7
Cristhian Traversi <crist.trav@gmail.com>
2023-08-09 02:08:25 -0400

    Se agrega la verificacion de acceso al formulario de Distritos

--- src/app/modulos/distritos/distritos-routing.module.ts
+++ src/app/modulos/distritos/distritos-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
 import { VistaDistritosComponent } from './vista-distritos/vista-distritos.component';
 import { DetalleDistritoComponent } from './detalle-distrito/detalle-distrito.component';
+import { SesionService } from '@servicios/sesion.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+
+const guardFn: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  if(!inject(SesionService).permisos.has(65)){
+    inject(NzNotificationService).create(
+      'warning',
+      '<strong>No autorizado</strong>',
+      'El usuario no tiene permisos para acceder al formulario de Distritos'
+    );
+    return false;
+  }else return true;
+}
 
 const routes: Routes = [
   { path: '', component: VistaDistritosComponent },
-  { path: ':id', component: DetalleDistritoComponent }
+  { path: ':id', component: DetalleDistritoComponent, canActivate: [guardFn]}
 ];
 
 @NgModule({
