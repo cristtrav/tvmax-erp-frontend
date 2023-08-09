e64d0c3fde1ac2b812e44650a5339395b567f29c
Cristhian Traversi <crist.trav@gmail.com>
2023-08-09 11:44:53 -0400

    Se agrega la verificacion de permisos para acceder al formulario de barrios

--- src/app/modulos/barrios/barrios-routing.module.ts
+++ src/app/modulos/barrios/barrios-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
 import { VistaBarriosComponent } from './vista-barrios/vista-barrios.component';
 import { DetalleBarrioComponent } from './detalle-barrio/detalle-barrio.component';
+import { SesionService } from '@servicios/sesion.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+
+const guardFn: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  if(!inject(SesionService).permisos.has(86)){
+    inject(NzNotificationService).create(
+      'warning',
+      '<strong>No autorizado</strong>',
+      'El usuario no tiene permisos para acceder al formulario de Barrios'
+    )
+    return false;
+  }else return true;
+}
 
 const routes: Routes = [
   {path: '', component: VistaBarriosComponent },
-  { path: ':id', component: DetalleBarrioComponent }
+  { path: ':id', component: DetalleBarrioComponent, canActivate: [guardFn] }
 ];
 
 @NgModule({
