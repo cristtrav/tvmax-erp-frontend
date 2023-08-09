9b8c5e49976efcb53d5f32532ca82c047912c7e0
Cristhian Traversi <crist.trav@gmail.com>
2023-08-09 02:03:14 -0400

    Se agrega la verificacion del permiso de acceso al formulario Departamentos

--- src/app/modulos/departamentos/departamentos-routing.module.ts
+++ src/app/modulos/departamentos/departamentos-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
 import { VistaDepartamentosComponent } from './vista-departamentos/vista-departamentos.component';
 import { DetalleDepartamentoComponent } from './detalle-departamento/detalle-departamento.component';
+import { SesionService } from '@servicios/sesion.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+
+const guardFn: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  if(!inject(SesionService).permisos.has(45)){
+    inject(NzNotificationService).create(
+      'warning',
+      '<strong>No autorizado</strong>',
+      'El usuario no tiene permisos para acceder al formulario de Departamentos'
+    );
+    return false;
+  }else return true;
+}
 
 const routes: Routes = [
   { path: '', component: VistaDepartamentosComponent },
-  { path: ':id', component: DetalleDepartamentoComponent}
+  { path: ':id', component: DetalleDepartamentoComponent, canActivate: [guardFn]}
 ];
 
 @NgModule({
