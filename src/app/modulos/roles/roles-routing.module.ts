c3b0a3d507bc3de6c11a443211e95b665995ae73
Cristhian Traversi <crist.trav@gmail.com>
2023-08-09 12:13:08 -0400

    Se agrega la verificacion de permisos para el acceso al formulario de Roles

--- src/app/modulos/roles/roles-routing.module.ts
+++ src/app/modulos/roles/roles-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
 import { DetalleRolComponent } from './detalle-rol/detalle-rol.component';
 import { VistaRolesComponent } from './vista-roles/vista-roles.component';
+import { SesionService } from '@servicios/sesion.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+
+const guardFn: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  if(!inject(SesionService).permisos.has(145)){
+    inject(NzNotificationService).create(
+      'warning',
+      '<strong>No autorizado</strong>',
+      'El usuario no tiene permisos para acceder al formulario de Roles'
+    );
+    return false;
+  }else return true;
+}
 
 const routes: Routes = [
   {path: '', component: VistaRolesComponent},
-  {path: ':idrol', component: DetalleRolComponent}
+  {path: ':idrol', component: DetalleRolComponent, canActivate: [guardFn]}
 ];
 
 @NgModule({
