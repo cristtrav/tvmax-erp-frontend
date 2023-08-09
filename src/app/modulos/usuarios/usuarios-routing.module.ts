fc8a257775453274be0158f7b37708149e5821e4
Cristhian Traversi <crist.trav@gmail.com>
2023-08-09 12:00:04 -0400

    Se agrega la verificacion de permisos al acceder al formulario de usuarios

--- src/app/modulos/usuarios/usuarios-routing.module.ts
+++ src/app/modulos/usuarios/usuarios-routing.module.ts
@@ -1,12 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
 import { VistaUsuariosComponent } from './vista-usuarios/vista-usuarios.component';
 import { DetalleUsuarioComponent } from './detalle-usuario/detalle-usuario.component';
 import { PermisosUsuarioComponent } from './permisos-usuario/permisos-usuario.component';
+import { SesionService } from '@servicios/sesion.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+
+const guardFn: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  if(!inject(SesionService).permisos.has(126)){
+    inject(NzNotificationService).create(
+      'warning',
+      '<strong>No autorizado</strong>',
+      'El usuario no tiene permisos para acceder al formulario de Usuarios'
+    );
+    return false;
+  }else return true;
+}
 
 const routes: Routes = [
   { path: '', component: VistaUsuariosComponent },
-  { path: ':idusuario', component: DetalleUsuarioComponent},
+  { path: ':idusuario', component: DetalleUsuarioComponent, canActivate: [guardFn]},
   { path: ':idusuario/permisos', component: PermisosUsuarioComponent }
 ];
 
