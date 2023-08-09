1b962979704e4c2b196b0f885b1ed934ff3f5874
Cristhian Traversi <crist.trav@gmail.com>
2023-08-09 01:44:09 -0400

    Se agrega Guard para acceder al detalle de grupos en el router de grupos

--- src/app/modulos/grupos/grupos-routing.module.ts
+++ src/app/modulos/grupos/grupos-routing.module.ts
@@ -1,14 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule, inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
 import { VistaGruposComponent } from './vista-grupos/vista-grupos.component';
 import { DetalleGrupoComponent } from './detalle-grupo/detalle-grupo.component';
+import { SesionService } from '@servicios/sesion.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+
+const guardFn: CanActivateFn = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  if(!inject(SesionService).permisos.has(6)) {
+    inject(NzNotificationService).create(
+      'warning',
+      '<strong>No autorizado</strong>',
+      'El usuario no tiene permisos para acceder al formulario'
+    );
+    return false;
+  } else return true;
+}
 
 const routes: Routes = [
   { 
-    path: '', component: VistaGruposComponent
+    path: '', component: VistaGruposComponent,    
   },
   {
-    path: ':id', component: DetalleGrupoComponent
+    path: ':id', component: DetalleGrupoComponent,
+    canActivate: [guardFn]
   }
 ];
 
