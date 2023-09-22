9e21ad7c407c4f3c341db7c9b9db8a42d1408312
Cristhian Traversi <crist.trav@gmail.com>
2023-09-21 20:08:25 -0400

    - Refactoring del codigo de vista-servicios - Se implemenenta la persistencia en URL de los parametros de la vista

--- src/app/modulos/servicios/vista-servicios/vista-servicios.component.ts
+++ src/app/modulos/servicios/vista-servicios/vista-servicios.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
-import { ServiciosService } from 'src/app/servicios/servicios.service';
-import { Servicio } from './../../../dto/servicio-dto';
-import { HttpErrorResponseHandlerService } from '../../../util/http-error-response-handler.service';
-import { NzTableQueryParams } from 'ng-zorro-antd/table';
+import { NzTableQueryParams, NzTableSortOrder } from 'ng-zorro-antd/table';
 import { HttpParams } from '@angular/common/http';
 import { GruposService } from '@servicios/grupos.service';
 import { forkJoin } from 'rxjs';
+import { finalize } from 'rxjs';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Servicio } from '@dto/servicio-dto';
+import { ServiciosService } from '@servicios/servicios.service';
+import { HttpErrorResponseHandlerService } from '@util/http-error-response-handler.service';
 
 @Component({
   selector: 'app-vista-servicios',
@@ -20,7 +22,13 @@ export class VistaServiciosComponent implements OnInit {
   pageSize: number = 10;
   pageIndex: number = 1;
   tableLoading: boolean = false;
-  sortStr: string | null = "+id";
+  //sortStr: string | null = "+id";
+  ordenarPor: string | null = 'id';
+  orden: NzTableSortOrder = 'ascend';
+
+  ordenarPorInicial: string | null = null;
+  ordenInicial: NzTableSortOrder = null;
+  filtroIdGrupoInicial: string[] = [];
 
   cantFiltrosAplicados: number = 0;
   drawerFiltrosVisible: boolean = false;
@@ -28,53 +36,86 @@ export class VistaServiciosComponent implements OnInit {
   textoBusqueda: string = '';
   timerBusqueda: any;
 
+  private readonly sortColumns: string[] = [
+    "id",
+    "descripcion",
+    "grupo",
+    "suscribible",
+    "precio"
+  ];
+  
+  readonly opNroRegistros: number[] = [10, 20, 30, 40, 50];
+
   constructor(
     private serviciosSrv: ServiciosService,
     private notif: NzNotificationService,
     private httpErrorRespSrv: HttpErrorResponseHandlerService,
-    private gruposSrv: GruposService
+    private gruposSrv: GruposService,
+    private aroute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
-    //this.cargarServicios();
-    this.cargarGruposFiltro();
+    this.procesarParametrosInicialesPagina();
+    this.cargarGruposFiltro();    
   }
 
-  private cargarServicios(): void {
+  private procesarParametrosInicialesPagina(){
+    const ordenarPor = this.aroute.snapshot.queryParamMap.get('ordenarPor');
+    this.ordenarPorInicial =
+      ordenarPor != null && this.sortColumns.includes(ordenarPor)
+      ? ordenarPor : 'id';
+
+    const orden = this.aroute.snapshot.queryParamMap.get('orden');
+    this.ordenInicial =
+      orden == 'ascend' || orden == 'descend'
+      ? orden : 'ascend';
+
+    const pagina = Number(this.aroute.snapshot.queryParamMap.get('pagina'));
+    this.pageIndex = pagina != null && !Number.isNaN(pagina) ? pagina : 1;
+
+    const nroRegistros = Number(this.aroute.snapshot.queryParamMap.get('nroRegistros'));
+    this.pageSize =
+      !Number.isNaN(nroRegistros) && this.opNroRegistros.includes(nroRegistros)
+      ? nroRegistros : this.opNroRegistros[0];
+
+    this.textoBusqueda = this.aroute.snapshot.queryParamMap.get('buscar') ?? '';
+    this.filtroIdGrupoInicial = this.aroute.snapshot.queryParamMap.getAll('idgrupo');
+  }
+
+  cargarServicios(): void {
     this.tableLoading = true;
     forkJoin({
       servicios: this.serviciosSrv.getServicios(this.getHttpQueryParams()),
       total: this.serviciosSrv.getTotalRegistros(this.getHttpQueryParams())
-    }).subscribe({
+    })
+    .pipe(finalize(() => this.tableLoading = false))
+    .subscribe({
       next: (respuesta) => {
         this.lstServicios = respuesta.servicios;
         this.totalRegisters = respuesta.total;
-        this.tableLoading = false;
       },
       error: (e) => {
         console.error('Error al cargar servicios', e);
         this.httpErrorRespSrv.process(e);
-        this.tableLoading = false;
       }
     });
   }
 
-  cargarGruposFiltro() {
+  private cargarGruposFiltro() {
     let params: HttpParams = new HttpParams();
     params = params.append('eliminado', 'false');
     params = params.append('sort', '+descripcion');
-    this.gruposSrv.getGrupos(params).subscribe({
+    this.gruposSrv.getGrupos(params)
+    .subscribe({
       next: (grupos) => {
-        const gruposChkbox: ICheckboxData[] = [];
-        for (let g of grupos) {
-          gruposChkbox.push(
-            {
-              label: `${g.descripcion}`,
-              value: `${g.id}`
-            }
-          );
-        }
-        this.filtrosGrupoChk = gruposChkbox;
+        this.filtrosGrupoChk = grupos.map(grupo => ({
+            label: `${grupo.descripcion}`,
+            value: `${grupo.id}`,
+            checked: this.filtroIdGrupoInicial.includes(`${grupo.id}`)
+          })
+        );
+        this.calcularCantFiltros();
       },
       error: (e) => {
         console.error('Error al cargar grupos', e);
@@ -84,96 +125,89 @@ export class VistaServiciosComponent implements OnInit {
   }
 
   eliminar(id: number | null): void {
-    if (id !== null) {
-      this.serviciosSrv.deleteServicio(id).subscribe(() => {
+    if (id !== null) this.serviciosSrv.deleteServicio(id).subscribe({
+      next: () => {
         this.cargarServicios();
         this.notif.create('success', 'Eliminado correctamente', '');
-      }, (e) => {
-        console.log('Error al eliminar Servicio');
-        console.log(e);
-        this.httpErrorRespSrv.handle(e);
-      });
-    }
+      },
+      error: (e) => {
+        console.log('Error al eliminar Servicio', e);
+        this.httpErrorRespSrv.process(e);
+      }
+    });
   }
 
   onTableQueryParamsChange(params: NzTableQueryParams) {
-    this.sortStr = this.buildSortStr(params.sort);
+    this.ordenarPor = params.sort.find(s => s.value != null)?.key ?? null;
+    this.orden = params.sort.find(s => s.value != null)?.value ?? null;
     this.pageIndex = params.pageIndex;
-    this.pageSize = params.pageSize;
+    this.pageSize = params.pageSize;    
     this.cargarServicios();
   }
 
-  private buildSortStr(sort: { key: string, value: any }[]): string | null {
-    for (let s of sort) {
-      if (s.value === 'ascend') return `+${s.key}`;
-      if (s.value === 'descend') return `-${s.key}`;
-    }
-    return null;
+  private buildSortStr(ordenarPor: string, orden: NzTableSortOrder): string {
+    return `${orden === 'descend' ? '-' : '+'}${ordenarPor}`;
   }
 
   private getHttpQueryParams(): HttpParams {
     var params: HttpParams = new HttpParams().append('eliminado', 'false');
+    
     params = params.append('limit', `${this.pageSize}`);
     params = params.append('offset', `${(this.pageIndex - 1) * this.pageSize}`);
-    if (this.sortStr) {
-      params = params.append('sort', this.sortStr);
-    }
-    if (this.existeFiltroGrupos()) {
-      this.getIdsGruposFiltro().forEach((idg: string) => {
-        params = params.append('idgrupo[]', idg);
+    if(this.ordenarPor && this.orden) params = params.append('sort', this.buildSortStr(this.ordenarPor, this.orden));
+    
+    if (this.filtrosGrupoChk.filter(fg => fg.checked).length > 0)
+      params = params.appendAll({
+        idgrupo: this.filtrosGrupoChk.filter(fg => fg.checked).map(fg => fg.value)
       });
-    }
-    if (this.textoBusqueda) {
-      params = params.append('search', this.textoBusqueda);
-    }
+    
+    if (this.textoBusqueda) params = params.append('search', this.textoBusqueda);
+    
+    this.procesarParametrosNavegacion();
     return params;
   }
 
+  private procesarParametrosNavegacion(){
+    const pageQueryParams: Params = {
+      pagina: this.pageIndex,
+      nroRegistros: this.pageSize
+    };
+
+    if(this.ordenarPor && this.orden){      
+      pageQueryParams['ordenarPor'] = this.ordenarPor;
+      pageQueryParams['orden'] = this.orden;
+    }
+
+    if (this.textoBusqueda) pageQueryParams['buscar'] = this.textoBusqueda;
+
+    if (this.filtrosGrupoChk.find(chk => chk.checked))
+      pageQueryParams['idgrupo'] = this.filtrosGrupoChk
+        .filter(chk => chk.checked)
+        .map(chk => chk.value);
+
+    this.router.navigate([], 
+      {
+        relativeTo: this.aroute,
+        queryParams: pageQueryParams,
+        replaceUrl: true
+      });
+  }
+
   filtroGrupoChange() {
-    console.log('el grupo cambio');
-    console.log(this.filtrosGrupoChk);
     this.calcularCantFiltros();
     this.cargarServicios();
   }
 
   limpiarFiltroGrupos() {
-    this.filtrosGrupoChk = this.filtrosGrupoChk.map((check: ICheckboxData) => {
-      check.checked = false;
-      return check;
-    });
+    this.filtrosGrupoChk.forEach(chk => chk.checked = false);
     this.calcularCantFiltros();
     this.cargarServicios();
   }
 
-  getIdsGruposFiltro(): string[] {
-    const idg: string[] = [];
-    for (let chk of this.filtrosGrupoChk) {
-      if (chk.checked) {
-        idg.push(chk.value);
-      }
-    }
-    return idg;
-  }
-
   calcularCantFiltros() {
-    let cant: number = 0;
-    for (let chkgrupo of this.filtrosGrupoChk) {
-      if (chkgrupo.checked) {
-        cant++;
-      }
-    }
+    let cant = 0;
+    if(this.filtrosGrupoChk.find(chk => chk.checked)) cant++
     this.cantFiltrosAplicados = cant;
-  }
-
-  existeFiltroGrupos(): boolean {
-    let existe: boolean = false;
-    for (let chk of this.filtrosGrupoChk) {
-      if (chk.checked) {
-        existe = true;
-        break;
-      }
-    }
-    return existe;
   }
 
   buscar() {
