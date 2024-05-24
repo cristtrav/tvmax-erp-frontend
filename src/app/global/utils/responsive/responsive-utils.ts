8af68b083d5c173b2d72f240c91ad1f7ec292cdb
Cristhian Traversi <crist.trav@gmail.com>
2024-05-24 19:32:53 -0400

    Agregando tamaño xs faltante a DEFAULT_FORM_SIZES

--- src/app/global/utils/responsive/responsive-utils.ts
+++ src/app/global/utils/responsive/responsive-utils.ts
@@ -1,8 +1,8 @@
 import { ResponsiveSizes } from "./responsive-sizes.interface";
 
 export class ResponsiveUtils{
-  static readonly DEFAULT_FORM_LABEL_SIZES: ResponsiveSizes = { sm: 7, xs: 24}
-  static readonly DEFALUT_FORM_CONTROL_SIZES: ResponsiveSizes = { sm: 15, xs: 24}
-  static readonly DEFAUT_FORM_SIZES: ResponsiveSizes = { xxl: 12, xl:14, lg: 16, md: 18, sm: 20 }
+  static readonly DEFAULT_FORM_LABEL_SIZES: ResponsiveSizes = { sm: 7, xs: 24};
+  static readonly DEFALUT_FORM_CONTROL_SIZES: ResponsiveSizes = { sm: 15, xs: 24};
+  static readonly DEFAUT_FORM_SIZES: ResponsiveSizes = { xxl: 12, xl:14, lg: 16, md: 18, sm: 20, xs: 24 };
   static readonly DEFAULT_FORM_ACTIONS_SIZES: ResponsiveSizes = { sm: 22, xs: 24 };
 }
