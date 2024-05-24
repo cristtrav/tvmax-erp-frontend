a26e0de52bca260282afbe1c9593b5d604323343
Cristhian Traversi <crist.trav@gmail.com>
2024-05-24 19:14:54 -0400

    Refactor workspace-layout

--- src/app/shared/workspace-layout/workspace-layout.component.ts
+++ src/app/shared/workspace-layout/workspace-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, HostListener, Input } from '@angular/core';
+import { ResponsiveSizes } from '@global-utils/responsive/responsive-sizes.interface';
+import { ResponsiveUtils } from '@global-utils/responsive/responsive-utils';
 
 @Component({
   selector: 'app-workspace-layout',
@@ -7,11 +9,20 @@ import { Component, HostListener, Input } from '@angular/core';
 })
 export class WorkspaceLayoutComponent {
 
+  readonly WORKSPACE_VERTICAL_GUTTER: ResponsiveSizes = { xs: 4, sm: 8, md: 8, lg: 8, xl: 8, xxl: 8};
+  readonly WORKSPACE_HORIZONTAL_GUTTER: ResponsiveSizes = { xs: 4, sm: 8, md: 8, lg: 8, xl: 8, xxl: 8};
+
   @Input()
   isContentPadded: boolean = true;
 
   @Input()
   isBackgroundVisible: boolean = true;
+
+  @Input()
+  isForm: boolean = false;
+
+  @Input()
+  formSizes: ResponsiveSizes = ResponsiveUtils.DEFAUT_FORM_SIZES;
 
   constructor(){
   }
--- src/app/shared/workspace-layout/workspace-layout.module.ts
+++ src/app/shared/workspace-layout/workspace-layout.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WorkspaceLayoutComponent } from './workspace-layout.component';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzColResponsiveSizesDirective } from '@global-directives/responsive/nz-col-responsive-sizes.directive';
 
 @NgModule({
   declarations: [WorkspaceLayoutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NzGridModule,
+    NzColResponsiveSizesDirective
   ],
   exports: [WorkspaceLayoutComponent]
 })
