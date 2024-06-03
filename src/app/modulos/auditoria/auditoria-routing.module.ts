598d7c3ba06ac28f580f3988e2e97dd1300f61d1
Cristhian Traversi <crist.trav@gmail.com>
2024-06-02 20:21:10 -0400

    Reorganizar el modulo Auditoria en las carpetas Pages y Components

--- src/app/modulos/auditoria/auditoria-routing.module.ts
+++ src/app/modulos/auditoria/auditoria-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { VistaAuditoriaComponent } from './vista-auditoria/vista-auditoria.component';
+import { VistaAuditoriaComponent } from './pages/vista-auditoria/vista-auditoria.component';
 
 const routes: Routes = [
   { path: '', component: VistaAuditoriaComponent }
